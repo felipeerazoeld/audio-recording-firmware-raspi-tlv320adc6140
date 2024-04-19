2ec3e8728444f12719c4937798560199831f50e0
felipe.erazo <felipe.erazo.eld@gmail.com>
2024-04-19 15:34:19 -0300

    sampling task of basic test

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,6 +1,63 @@
 import sys
-sys.path.append("/home/root/WS/audio-recording-firmware-raspi-tlv320adc6140/")
-import TLV320ADC
-adc1 = TLV320ADC.TLV320ADC()
-import autorecord as ar
-ar.setup_adc(adc1,30.0,27.0)
+import json
+# Opening JSON file
+f = open('settings.json')
+data = json.load(f)
+f.close()
+#
+sys.path.append(str(data['GLOBAL']['path_append']))
+
+import pyaudio
+
+p = pyaudio.PyAudio()
+info = p.get_host_api_info_by_index(0)
+numdevices = info.get('deviceCount')
+for i in range(0, numdevices):
+    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+
+import wave
+
+chunk = 1024  # Record in chunks of 1024 samples
+sample_format = pyaudio.paInt32  # 16 bits per sample
+channels = 4
+fs = 192000  # Record at 44100 samples per second
+seconds = 3
+filename = "output.wav"
+
+p = pyaudio.PyAudio()  # Create an interface to PortAudio
+
+print('Recording')
+
+stream = p.open(format=sample_format,
+                channels=channels,
+                rate=fs,
+                frames_per_buffer=chunk,
+                input_device_index=2,
+                input=True)
+
+frames = []  # Initialize array to store frames
+
+# Store data in chunks for 3 seconds
+for i in range(0, int(fs / chunk * seconds)):
+    data = stream.read(chunk)
+    frames.append(data)
+
+# Stop and close the stream 
+
+
+stream.stop_stream()
+stream.close()
+# Terminate the PortAudio interface
+p.terminate()
+
+print('Finished recording')
+
+# Save the recorded data as a WAV file
+wf = wave.open(filename, 'wb')
+wf.setnchannels(channels)
+wf.setsampwidth(p.get_sample_size(sample_format))
+wf.setframerate(fs)
+wf.writeframes(b''.join(frames))
+wf.close()
+
