3800856a610bd944d983390cdd7cb49e71937bff
felipe.erazo.eld <felipe.erazo.eld@gmail.com>
2024-04-23 16:58:53 -0300

    if TI USB Audio 2.0: them write in Json file and reload data variable

--- tests/test_0_collect_sample.py
+++ tests/test_0_collect_sample.py
@@ -14,12 +14,26 @@ p = pyaudio.PyAudio()
 info = p.get_host_api_info_by_index(0)
 numdevices = info.get('deviceCount')
 for i in range(0, numdevices):
-    if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
-        print("Input Device id ", i, " - ", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+    try:
+        if (p.get_device_info_by_host_api_device_index(0, i).get('maxInputChannels')) > 0:
+            print("Input Device id ", i, "-", p.get_device_info_by_host_api_device_index(0, i).get('name'))
+        if str(p.get_device_info_by_host_api_device_index(0, i).get('name'))=="TI USB Audio 2.0: - (hw:0,0)":
+            #dev_str=str(p.get_device_info_by_host_api_device_index(0, i).get('name'))
+            #print(dev_str.split())
+            with open('./conf/settings.json', 'r+') as f:
+                data = json.load(f)
+                data['ADC']['INPUT_DEV_ID'] = str(i) 
+                f.seek(0)
+                json.dump(data, f, indent=4)
+                data = json.load(f)
+                f.truncate()
+                f.close()
+    except Exception as e:
+       print("*E: "+str(e))
 
 import time
 timestr = time.strftime("%Y%m%d-%H%M%S")
-print(timestr)
+#print(timestr)
 
 import wave
 
